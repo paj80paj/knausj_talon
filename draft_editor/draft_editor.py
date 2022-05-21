c9b72b74ddf1beb40f970bffa01f15bd9ef5a5b8
Paul Jones <paul.alexander.jones@gmail.com>
2022-05-21 15:34:36 +0100

    changes to draft editor

--- draft_editor/draft_editor.py
+++ draft_editor/draft_editor.py
@@ -106,6 +106,7 @@ def close_editor(submit_draft: bool):
     remove_tag("user.draft_editor_active")
     actions.edit.select_all()
     selected_text = actions.edit.selected_text()
+    actions.user.copy(selected_text)
     actions.edit.delete()
     actions.app.tab_close()
     actions.user.switcher_focus_window(original_window)
