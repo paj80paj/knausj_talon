a209afc161db26add3b971963c600a057e632330
paj80paj <102916005+paj80paj@users.noreply.github.com>
2023-01-02 13:41:46 +0000

    my_contacts functionality

---
+++ core/my_contacts/my_contacts.py
@@ -0,0 +1,21 @@
+from talon import Context, Module
+from urllib.parse import quote_plus
+from ..user_settings import get_list_from_csv
+# /Users/paul.jones/.talon/user/MyTalonConfig/contact_list.py
+# f1\n in /Users/paul.jones/.talon/user/knausj_talon/core/websites_and_search_engines/websites_and_search_engines.py
+mod = Module()
+mod.list("my_note_groups", desc="My note Groups")
+
+ctx = Context()
+
+default_contacts = {
+     "default": "default",
+ } 
+
+my_note_groups = get_list_from_csv(
+    "my_contacts.csv",
+    headers=("Replacement","Original"),
+         default="default_contacts") #key_first=True)
+
+
+ctx.lists["user.my_note_groups"] = my_note_groups
